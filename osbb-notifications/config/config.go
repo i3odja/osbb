c8a3afdf589e851a17b7246dc134587e52c998b5
i3odja <wbodjaw@gmail.com>
2020-07-24 23:22:46 +0300

    Add simple connection to DB

--- osbb-notifications/config/config.go
+++ osbb-notifications/config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/i3odja/osbb/notifications/storage"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	PostgresHost string `envconfig:"POSTGRES_HOST" required:"true"`
+	PostgresPort int    `envconfig:"POSTGRES_PORT" required:"true"`
+	PostgresUser string `envconfig:"POSTGRES_USER" required:"true"`
+	PostgresPass string `envconfig:"POSTGRES_PASSWORD" required:"true"`
+	PostgresDB   string `envconfig:"POSTGRES_DB" required:"true"`
+}
+
+// NewConfig() create new configuration for application
+func NewConfig() (*Config, error) {
+	var config Config
+	// Read all environment variables and fill config structure with them
+	err := envconfig.Process("", &config)
+	if err != nil {
+		return nil, fmt.Errorf("envconfig error %w", err)
+	}
+
+	return &config, nil
+}
+
+// DBConfig get configuration for Postgres Database
+func (c *Config) DBConfig(ctx context.Context) (*storage.DBConfig, error) {
+	return &storage.DBConfig{
+		Host:     c.PostgresHost,
+		Port:     c.PostgresPort,
+		User:     c.PostgresUser,
+		Password: c.PostgresPass,
+		DBName:   c.PostgresDB,
+	}, nil
+}
--- osbb-notifications/go.mod
+++ osbb-notifications/go.mod
@@ -7,10 +7,13 @@ replace github.com/i3odja/osbb/contracts => ./../osbb-contracts
 replace github.com/i3odja/osbb/shared => ./../osbb-shared
 
 require (
+	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/websocket v1.4.2
 	github.com/i3odja/osbb/contracts v0.0.0-20200718175326-fcc017c8c09b
 	github.com/i3odja/osbb/shared v0.0.0-00010101000000-000000000000
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.7.1
 	github.com/sirupsen/logrus v1.6.0
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 	google.golang.org/grpc v1.30.0
