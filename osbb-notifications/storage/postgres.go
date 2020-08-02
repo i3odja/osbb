2b5788e285118f97807910e606d2be6f1dda517a
i3odja <wbodjaw@gmail.com>
2020-08-02 08:50:59 +0300

    Remove the commented code

--- osbb-notifications/storage/postgres.go
+++ osbb-notifications/storage/postgres.go
@@ -42,28 +42,3 @@ func ConnectToDB(pg *DBConfig) (*sql.DB, error) {
 func getDBConfigString(pg *DBConfig) string {
 	return fmt.Sprintf(pgStr, pg.Host, pg.Port, pg.User, pg.Password, pg.DBName)
 }
-
-//const queryInsert = `INSERT INTO public.notifications(id, message) VALUES ($1,$2)`
-//
-////Notifications{} contains pointer to database
-//type Notifications struct {
-//	db *sql.DB
-//}
-//
-//// NewNotifications() returns Notifications with db
-//func NewNotifications(data *sql.DB) *Notifications {
-//	return &Notifications{db: data}
-//}
-//
-//// Add() adds new user to database
-//func (n *Notifications) Add(message string) error {
-//	ui, err := uuid.NewRandom()
-//
-//	if err != nil {
-//		return fmt.Errorf("uuid.NewRandom error: %w", err)
-//	}
-//
-//	_, err = n.db.Exec(queryInsert, ui, message)
-//
-//	return err
-//}
