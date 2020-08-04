16ee33822df6783854cfdb57850ae11ae577d339
i3odja <wbodjaw@gmail.com>
2020-08-04 18:14:30 +0300

    Add Get() method for getting message from db

--- osbb-notifications/service/notifications.go
+++ osbb-notifications/service/notifications.go
@@ -8,6 +8,7 @@ import (
 )
 
 const queryInsert = `INSERT INTO public.notifications(id, message) VALUES ($1,$2)`
+const querySelect = `SELECT message FROM public.notifications WHERE id=$1`
 
 //Notifications{} contains pointer to database
 type Notifications struct {
@@ -30,4 +31,19 @@ func (n *Notifications) Add(message string) error {
 	_, err = n.db.Exec(queryInsert, ui, message)
 
 	return err
+}
+
+// Get() gets user's message by user id
+func (n *Notifications) Get(id string) (string, error) {
+	var message string
+
+	err := n.db.QueryRow(querySelect, id).Scan(&message)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", fmt.Errorf("ID does not exist")
+		}
+		return "", err
+	}
+
+	return message, nil
 }
